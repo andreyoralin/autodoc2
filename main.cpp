a4731a4953a813024f0686bf54279f43955cc12e
andreyoralin <107479073+andreyoralin@users.noreply.github.com>
2024-06-13 10:22:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
+/*! \file main.cpp
+ *  \brief The best
+ *
+ *  I am the best
+ */
 
-int main(int argc, char** argv) {
-	return 0
+/*! \fn void theBestFunction(int)
+ * first doc
+ */
+void theBestFunction(int ever)
 {
+    doThings();
+    /*!
+     * Does some more things
+     */
+    doMoreThings();
+    /*!
+     * Checks that the things it does are the best
+     */
+    checkBest();
+}
